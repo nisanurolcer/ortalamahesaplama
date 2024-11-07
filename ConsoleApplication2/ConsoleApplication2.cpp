2d56760a8d5ba72219a965c3884f2db38848f303
nisa <nisa@WINDOWS-5P4BNDK>
2024-11-07 23:41:19 +0300

    Proje dosyası ekle.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int vıze = 50;
+	int fınal = 60;
+	int odev_notu = 40;
+	int ortalama;
+	ortalama = (vıze + fınal + odev_notu) / 3;
+		cout << "ortalama = " << ortalama;
+		return 0;
+}
